5cc8f044ab2280e5294b55cd2a995ef69fcdaff4
Steve Marshall <steve@nascentguruism.com>
2013-02-13 16:14:58 +0000

    Remove unneeded-for-now dependency.

--- metadata.rb
+++ metadata.rb
@@ -6,4 +6,3 @@ long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           "0.0.1"
 supports          "mac_os_x"
 depends           "pivotal_workstation"
-depends           "dmg"
