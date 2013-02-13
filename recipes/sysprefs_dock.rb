f1387140ba8665724b2587a0e77fb95e0981f066
Steve Marshall <steve@nascentguruism.com>
2013-02-13 16:44:39 +0000

    Manage persistent dock items.

--- recipes/sysprefs_dock.rb
+++ recipes/sysprefs_dock.rb
@@ -23,30 +23,97 @@ pivotal_workstation_defaults "Set magnified dock size" do
   notifies :run, "execute[restart Dock]"
 end
 
-# TODO: Persistent apps/folders
-# TODO: Work out why clearing the dock doesn’t work
-# pivotal_workstation_defaults "Clear the dock" do
-#   domain 'com.apple.dock'
-#   key 'persistent-apps'
-#   action :delete
-#   notifies :run, "execute[restart Dock]"
-# end
-# 
-# [
-#   "/Applications/Safari.app",
-#   "/Applications/Mail.app",
-#   "/Applications/Calendar.app",
-#   "#{ENV['HOME']}/Applications/Things.app",
-#   "/Applications/Notes.app",
-#   "/Applications/Messages.app",
-#   "#{ENV['HOME']}/Applications/Adium.app",
-#   "#{ENV['HOME']}/Applications/Flint.app",
-#   "#{ENV['HOME']}/Applications/Twitter.app",
-#   "#{ENV['HOME']}/Applications/TextMate.app",
-#   "/Applications/Utilities/Terminal.app",
-# ].each do |app_path|
-#   nascent_workstation_dock_item "Add #{app_path} to dock" do
-#     path app_path
-#     notifies :run, "execute[restart Dock]"
-#   end
-# end
+persistent_apps = [
+  "/Applications/Safari.app",
+  "/Applications/Mail.app",
+  "/Applications/Calendar.app",
+  "#{ENV['HOME']}/Applications/Things.app",
+  "/Applications/Notes.app",
+  "/Applications/Messages.app",
+  "#{ENV['HOME']}/Applications/Adium.app",
+  "#{ENV['HOME']}/Applications/Flint.app",
+  "#{ENV['HOME']}/Applications/Twitter.app",
+  "#{ENV['HOME']}/Applications/TextMate.app",
+  "/Applications/Utilities/Terminal.app",
+].map { |app_path|
+  "<dict>
+    <key>tile-data</key>
+    <dict>
+      <key>file-data</key>
+      <dict>
+        <key>_CFURLString</key>
+        <string>#{app_path}/</string>
+        <key>_CFURLStringType</key>
+        <integer>0</integer>
+      </dict>
+    </dict>
+  </dict>"
+}
+pivotal_workstation_defaults "Set persistent apps" do
+  domain 'com.apple.dock'
+  key 'persistent-apps'
+  array persistent_apps
+  notifies :run, "execute[restart Dock]"
+end
+
+dock_display_as = {
+  :stack => 0,
+  :folder => 1,
+}
+dock_sort_by = {
+  :name => 0,
+  :date_added => 2,
+  :date_modified => 3,
+  :date_created => 4,
+  :kind => 5,
+}
+dock_view_contents_as = {
+  :automatic => 0,
+  :fan => 1,
+  :grid => 2,
+  :list => 3
+}
+persistent_others = {
+  "#{ENV['HOME']}/Downloads" => {
+    :displayas => dock_display_as[:stack],
+    :arrangement => dock_sort_by[:date_added],
+    :showas => dock_view_contents_as[:automatic],
+  },
+  "#{ENV['HOME']}/Dropbox/Documents/Home" => {
+    :displayas => dock_display_as[:folder],
+    :arrangement => dock_sort_by[:name],
+    :showas => dock_view_contents_as[:automatic],
+  },
+  "#{ENV['HOME']}/Dropbox/Documents/Work" => {
+    :displayas => dock_display_as[:folder],
+    :arrangement => dock_sort_by[:name],
+    :showas => dock_view_contents_as[:automatic],
+  },
+}.map { |item_path, settings|
+  "<dict>
+    <key>tile-type</key>
+    <string>directory-tile</string>
+    <key>tile-data</key>
+    <dict>
+      <key>arrangement</key>
+      <integer>#{settings[:arrangement]}</integer>
+      <key>displayas</key>
+      <integer>#{settings[:displayas]}</integer>
+      <key>showas</key>
+      <integer>#{settings[:showas]}</integer>
+      <key>file-data</key>
+      <dict>
+        <key>_CFURLString</key>
+        <string>#{item_path}/</string>
+        <key>_CFURLStringType</key>
+        <integer>0</integer>
+      </dict>
+    </dict>
+  </dict>"
+}
+pivotal_workstation_defaults "Set persistent others" do
+  domain 'com.apple.dock'
+  key 'persistent-others'
+  array persistent_others
+  notifies :run, "execute[restart Dock]"
+end
--- resources/dock_item.rb
+++
@@ -1,9 +0,0 @@
-actions :add
-
-attribute :description, :kind_of => String, :name_attribute => true
-attribute :path, :kind_of => String, :default => nil
-
-def initialize(name, run_context=nil)
-  super
-  @action = :add
-end
