a9ee5de7e5155672d8b0e7a1698ad466621968e4
Steve Marshall <steve@nascentguruism.com>
2013-02-17 21:47:46 +0000

    Set user profile image

--- recipes/sysprefs_users.rb
+++ recipes/sysprefs_users.rb
@@ -1,5 +1,17 @@
-# TODO: User profile image
 # TODO: Create admin user and de-auth current user?
+
+# Set profile image
+# TODO: Extract profile image from iCloud contacts?
+cookbook_file "#{Chef::Config[:file_cache_path]}/user_image.jpeg" do
+  source "user_image.jpeg"
+  owner WS_USER
+end
+template "#{Chef::Config[:file_cache_path]}/jpegphoto.dsimport" do
+  source "user_image.dsimport.erb"
+  owner WS_USER
+end
+execute("dscl . delete /Users/#{WS_USER} JPEGPhoto")
+execute("dsimport #{Chef::Config[:file_cache_path]}/jpegphoto.dsimport /Local/Default M")
 
 login_items = [
   "/Applications/iTunes.app/Contents/MacOS/iTunesHelper.app",
@@ -8,7 +20,6 @@ login_items = [
 ].map { |app_path| 
   %Q{make login item at end with properties \{path:"#{app_path}"\}}
 }.join("\n")
-
 ruby_block "Reset login items" do
   block do
     system(
