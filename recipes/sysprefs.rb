4b5ac524ac16a58b4618e722e506eba4775e8f7e
Steve Marshall <steve@nascentguruism.com>
2013-02-17 22:09:58 +0000

    Ignore CDs and DVDs appearing.

--- recipes/sysprefs.rb
+++ recipes/sysprefs.rb
@@ -3,10 +3,11 @@ include_recipe "nascent_workstation::utils"
 include_recipe "nascent_workstation::sysprefs_general"
 include_recipe "nascent_workstation::sysprefs_desktop"
 include_recipe "nascent_workstation::sysprefs_dock"
-# TODO: Misson control
+# Unneeded: Misson control
 include_recipe "nascent_workstation::sysprefs_language"
 include_recipe "nascent_workstation::sysprefs_security"
 # TODO: Notifications
+include_recipe "nascent_workstation::sysprefs_cds_and_dvds"
 # Unneeded?: CDs & DVDs; Displays
 include_recipe "nascent_workstation::sysprefs_energy"
 include_recipe "nascent_workstation::sysprefs_keyboard"
---
+++ recipes/sysprefs_cds_and_dvds.rb
@@ -0,0 +1,10 @@
+%w{
+  dvd.video
+  cd.music
+  cd.photo
+}.each do |property|
+  execute "Ignore #{property} appearing" do
+    command "defaults write com.apple.digihub com.apple.digihub.#{property}.appeared -dict action 1"
+    user WS_USER
+  end
+end
