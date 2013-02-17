c685804c59af039c74a532f904d1bda764ad4f0e
Steve Marshall <steve@nascentguruism.com>
2013-02-17 11:41:24 +0000

    Configure Sophos to meet my ultra-minimal sensibilities.

--- recipes/apps.rb
+++ recipes/apps.rb
@@ -2,3 +2,6 @@ include_recipe "pivotal_workstation::adium"
 include_recipe "pivotal_workstation::dropbox"
 include_recipe "pivotal_workstation::textmate"
 include_recipe "pivotal_workstation::things"
+
+# Configure work crap
+include_recipe "nascent_workstation::sophos"
