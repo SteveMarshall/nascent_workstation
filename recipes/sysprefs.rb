a62fa30393ea47346b6887337bd0b03960fed5ba
Steve Marshall <steve@nascentguruism.com>
2013-02-17 23:39:58 +0000

    Correct comment.

--- recipes/sysprefs.rb
+++ recipes/sysprefs.rb
@@ -8,7 +8,7 @@ include_recipe "nascent_workstation::sysprefs_language"
 include_recipe "nascent_workstation::sysprefs_security"
 # TODO: Notifications
 include_recipe "nascent_workstation::sysprefs_cds_and_dvds"
-# Unneeded?: CDs & DVDs; Displays
+# Unneeded?: Displays
 include_recipe "nascent_workstation::sysprefs_energy"
 include_recipe "nascent_workstation::sysprefs_keyboard"
 # Unneeded?: Mouse; Trackpad; Print & Scan; Sound
