1be63169684c10a215e4a67db32b380b0188ce07
Steve Marshall <steve@nascentguruism.com>
2013-02-19 09:13:45 +0000

    Tweak comments.

--- recipes/sophos.rb
+++ recipes/sophos.rb
@@ -1,3 +1,4 @@
+# TODO: Only do this if Sophos is already installed
 pivotal_workstation_defaults "Hide Sophos's menubar icon" do
   domain 'com.sophos.ui'
   key 'displayMenuIcon'
--- recipes/utils.rb
+++ recipes/utils.rb
@@ -1,15 +1,15 @@
 execute "restart Dock" do
   command "killall Dock"
   action :nothing
-  ignore_failure true # SystemUIServer is not running if not logged in
+  ignore_failure true
 end
 execute "restart Finder" do
   command "killall Finder"
   action :nothing
-  ignore_failure true # SystemUIServer is not running if not logged in
+  ignore_failure true
 end
 execute "restart SystemUIServer" do
   command "killall -HUP SystemUIServer"
   action :nothing
-  ignore_failure true # SystemUIServer is not running if not logged in
+  ignore_failure true
 end
