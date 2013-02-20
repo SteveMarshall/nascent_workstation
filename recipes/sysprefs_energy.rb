f249baa0252b4a18e488a72bd2f132e9fe08a335
Steve Marshall <steve@nascentguruism.com>
2013-02-20 12:41:16 +0000

    Fix slow wake for Retina MBP

--- recipes/sysprefs_energy.rb
+++ recipes/sysprefs_energy.rb
@@ -4,3 +4,9 @@ end
 execute "Set adapter power settings" do
   command "pmset -c displaysleep 30 sleep 0"
 end
+
+# HACK: Fix slow wake for Retina MBP
+# http://www.ewal.net/2012/09/09/slow-wake-for-macbook-pro-retina/
+
+standbydelay = 12 * 60 * 60
+execute "pmset -a standbydelay #{standbydelay}"
