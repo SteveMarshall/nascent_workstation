90640466132ef05d4afa7ed73f32ea6bc83761f1
Steve Marshall <steve@nascentguruism.com>
2014-01-28 01:03:06 +0000

    Only run perl/homebrew if we have stuff to run!

--- recipes/homebrew.rb
+++ recipes/homebrew.rb
@@ -1,13 +1,19 @@
-node['homebrew']['taps'].each do |tap|
-  homebrew_tap tap
-  tap_dir = tap.gsub('/', '-')
-  execute "fix #{tap} permissions" do
-    command "chown -R #{node['current_user']} /usr/local/Library/Taps/#{tap_dir}"
+if node['homebrew']
+  if node['homebrew']['taps']
+    node['homebrew']['taps'].each do |tap|
+      homebrew_tap tap
+      tap_dir = tap.gsub('/', '-')
+      execute "fix #{tap} permissions" do
+        command "chown -R #{node['current_user']} /usr/local/Library/Taps/#{tap_dir}"
+      end
+    end
   end
-end
 
-node['homebrew']['packages'].each do |pkg, opt|
-  package pkg do
-    options opt
+  if node['homebrew']['packages']
+    node['homebrew']['packages'].each do |pkg, opt|
+      package pkg do
+        options opt
+      end
+    end
   end
 end
--- recipes/perl.rb
+++ recipes/perl.rb
@@ -1,7 +1,9 @@
-node['perl']['modules'].each do |pkg|
-  # HACK: perl::cpan_module doesn't install as current_user
-  execute "install-#{pkg}" do
-   command "su #{node['current_user']} -l -c '#{node['perl']['cpanm']['path']} --notest #{pkg}'"
-   not_if %Q{su #{node['current_user']} -l -c "perl -m#{pkg} -e ''"}
+if node['perl'] and node['perl']['modules']
+  node['perl']['modules'].each do |pkg|
+    # HACK: perl::cpan_module doesn't install as current_user
+    execute "install-#{pkg}" do
+     command "su #{node['current_user']} -l -c '#{node['perl']['cpanm']['path']} --notest #{pkg}'"
+     not_if %Q{su #{node['current_user']} -l -c "perl -m#{pkg} -e ''"}
+    end
   end
 end
