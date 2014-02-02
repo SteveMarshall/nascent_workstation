d78fd88c195587eb5a5e40d4d65de1f7ae874248
Steve Marshall <steve@nascentguruism.com>
2014-02-02 13:40:22 +0000

    Install TextMate.

--- attributes/apps.rb
+++ attributes/apps.rb
@@ -10,8 +10,9 @@ node.default["transmit"]["destination"]="#{ENV['HOME']}/Applications"
 node.default["hex_color_picker"]["source"] = "http://wafflesoftware.net/hexpicker/download/"
 node.default["hex_color_picker"]["destination"] = "#{ENV['HOME']}/Library/ColorPickers/"
 
-# node.default["textmate"]["destination"]="#{ENV['HOME']}/Applications"
-# node.default["textmate"]["cmd_destination"] = "#{ENV['HOME']}/bin"
+node.default["textmate"]["source"]="https://github.com/textmate/textmate/releases/download/v2.0-alpha.9503/TextMate_2.0-alpha.9503.tbz"
+node.default["textmate"]["destination"]="#{ENV['HOME']}/Applications"
+node.default["textmate"]["cmd_destination"] = "#{ENV['HOME']}/bin"
 # node.default["things"]["destination"]="#{ENV['HOME']}/Applications"
 
 node.default["quicklook"]["markdown"]["source"] = "https://github.com/downloads/toland/qlmarkdown/QLMarkdown-1.3.zip"
--- recipes/textmate.rb
+++ recipes/textmate.rb
@@ -1,4 +1,15 @@
-# include_recipe "pivotal_workstation::textmate"
+tar_extract node.textmate.source do
+  target_dir node.textmate.destination
+  creates "#{node.textmate.destination}/TextMate.app"
+  user node['current_user']
+  group 'staff'
+  compress_char 'j'
+end
+
+execute "link textmate" do
+  command "ln -s #{node.textmate.destination}/TextMate.app/Contents/Resources/mate #{node.textmate.cmd_destination}/mate"
+  not_if "test -e #{node.textmate.cmd_destination}/mate"
+end
 
 directory "#{ENV['HOME']}/Library/Application Support/TextMate" do
   action :delete
