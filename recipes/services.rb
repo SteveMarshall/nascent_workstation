a351d3d440d318e1e336a8b0e7741b812eec212b
Steve Marshall <steve@nascentguruism.com>
2014-02-13 20:10:13 +0000

    Write to something we can control… for now.

--- recipes/services.rb
+++ recipes/services.rb
@@ -4,12 +4,13 @@ git "#{ENV['HOME']}/Library/Services/open-in-chrome.workflow" do
   user node['current_user']
 end
 
-git "#{Chef::Config[:file_cache_path]}/resize-services" do
+git "#{ENV['HOME']}/Library/Services/resize-services" do
   repo "git@github.com:SteveMarshall/osx-window-resize-services.git"
   action :sync
+  user node['current_user']
 end
 
 execute "install_resizers" do
-  cwd "#{Chef::Config[:file_cache_path]}/resize-services"
+  cwd "#{ENV['HOME']}/Library/Services/resize-services"
   command %Q{sudo su #{node['current_user']} -l -c "make install"}
 end
