f279c8f2280508a9433f8a8c8441ab073234cb9f
Steve Marshall <steve@stevemarshall.com>
2015-11-27 19:12:27 +0000

    Drop Safari extensions I don't use anymore

--- recipes/safari.rb
+++ recipes/safari.rb
@@ -79,34 +79,17 @@ cookbook_file "#{ENV['HOME']}/Library/Safari/Extensions/Extensions.plist" do
   source "safari_extensions_skeleton.plist"
   owner node['current_user']
 end
-# TODO: Identify other installed extensions
 extensions = {
-  # TODO: Detox?
-  # TODO: Jargone
   "JSON Formatter" => {
     :source => "http://cloud.github.com/downloads/rfletcher/safari-json-formatter/JSON_Formatter-1.1.safariextz",
-    :checksum => "aab8dda4486079efa57fe22f7af704a9baf7d2b5"
-  },
-  "Media Center" => {
-    :source => "http://hoyois.github.io/safariextensions/builds/MediaCenter-2.2.safariextz",
-    :checksum => "f481ab3adc67785a61e66c348f7eca88"
   },
   "Readability" => {
     :source => "https://www.readability.com/extension/safari",
-    :checksum => "73c0c5f5c27354729c511b4cc3cb8d9c9014529b",
     :removed_default_toolbar_items => [
       "com.readability.safari-6K2928F88K readability_save",
       "com.readability.safari-6K2928F88K readability_kindle",
       "com.readability.safari-6K2928F88K readability_read"
     ]
-  },
-  "SafariKeywordSearch" => {
-    :source => "http://safarikeywordsearch.aurlien.net/SafariKeywordSearch.safariextz",
-    :checksum => "fd651e4b284b339d38676bf84b58b9b88523a14d"
-  },
-  "YouTube5" => {
-    :source => "http://www.verticalforest.com/youtube5/YouTube5.safariextz",
-    :checksum => "f7485d795ad8cb8153ad82a834d465f89ca63f6f"
   },
 }
 
@@ -115,7 +98,6 @@ extensions.each do |name, download|
   if download[:source] =~ /\.zip$/
     remote_file "#{Chef::Config[:file_cache_path]}/#{name}.zip" do
       source download[:source]
-      checksum download[:checksum]
       owner node['current_user']
     end
 
@@ -130,7 +112,6 @@ extensions.each do |name, download|
   else
     remote_file "#{ENV['HOME']}/Library/Safari/Extensions/#{name}.safariextz" do
       source download[:source]
-      checksum download[:checksum]
       owner node['current_user']
       mode 00777
       # HACK: Set header to avoid https://tickets.opscode.com/browse/CHEF-5010
@@ -193,15 +174,8 @@ end
 
 
 {
-  "ExtensionSettings-com.hoyois.safari.mediacenter-GY5KR7239Q" => '{
-    airplayHostname = "\\"apple-tv.local\\"";
-  }',
   "ExtensionSettings-com.readability.safari-6K2928F88K" => '{
     first_run_complete = true;
-  }',
-  "ExtensionSettings-com.verticalforest.youtube5-B7HHQRRC44" => '{
-    enableVimeo = false;
-    enableYouTube = true;
   }',
 }.each do |property, values|
   execute "Set Safari extension properties" do
@@ -224,21 +198,3 @@ end
 file "/Library/Internet\ Plug-Ins/flashplayer.xpt" do
   action :delete
 end
-# TODO: Safari Keyword Search settings in sqlite3 DB at ~/Library/Safari/LocalStorage/safari-extension_net.aurlien.safarikeywordsearch-5ysse2v8p3_0.localstorage
-# CREATE TABLE ItemTable (key TEXT UNIQUE ON CONFLICT REPLACE, value BLOB NOT NULL ON CONFLICT FAIL);
-# {
-#   :a => "https://www.amazon.co.uk/s/?field-keywords=@@@",
-#   :bug => "http://eanjira/browse/@@@",
-#   :cpan => "http://search.cpan.org/search?mode=all&query=@@@",
-#   :ddg => "http://duckduckgo.com/?q=@@@",
-#   :down => "http://downforeveryoneorjustme.com/@@@",
-#   :g => "http://www.google.com/search?q=@@@",
-#   :imdb => "http://imdb.com/find?s=all&q=@@@",
-#   :maps => "http://maps.google.co.uk/maps?oi=map&q=@@@",
-#   :wa => "http://www.wolframalpha.com/input/?i=@@@",
-#   :w => "http://en.wikipedia.org/wiki/Special:Search/%%%",
-#   :y => "http://youtube.com/results?search_query=@@@"
-#   # ___keywordExpansionsAreSaved|1
-#   # ___defaultUpgraded|1
-#   :Default => "ddg"
-# }
